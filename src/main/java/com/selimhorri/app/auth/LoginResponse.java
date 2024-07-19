fdb9d01c48140c5e5326c4778fccc73300e9626c
SelimHorri <horri.slim@gmail.com>
2024-07-20 00:49:27 +0100

    Register candidate for reflection for native execution

--- src/main/java/com/selimhorri/app/auth/LoginResponse.java
+++ src/main/java/com/selimhorri/app/auth/LoginResponse.java
@@ -1,5 +1,8 @@
 package com.selimhorri.app.auth;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection(registerFullHierarchy = true)
 public record LoginResponse(String accessToken) {}
 
 
